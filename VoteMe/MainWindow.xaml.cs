867864773b7f12bcc56a8227c265352b9a863df9
Forelock10@gmail.com <Forelock10@gmail.com>
2022-05-27 19:22:48 +0300

    Добавьте файлы проекта.

---
+++ VoteMe/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VoteMe.Pages;
+
+namespace VoteMe
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+
+        }
+
+        private void btStart_Click(object sender, RoutedEventArgs e)
+        {
+            main.Content = new QuestionPage();
+        }
+
+        private void btRez_Click(object sender, RoutedEventArgs e)
+        {
+            main.Content = new ResultPage();
+        }
+    }
+}
